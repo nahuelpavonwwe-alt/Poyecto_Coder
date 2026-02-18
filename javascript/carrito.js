3f88d0c801973f59e0fc703778f6fa75b91dcf4d
nahuelpavonwwe-alt <nahuelpavonwwe@gmail.com>
2026-02-18 19:52:19 -0300

    Creacion de los botones de sumar eliminar y restar items del carrito

--- javascript/carrito.js
+++ javascript/carrito.js
@@ -11,7 +11,69 @@ function guardarCarrito() {
     localStorage.setItem("carrito", JSON.stringify(carrito));
 }
 
+function aumentarCantidad(id) {
+    const producto = carrito.find(item => item.id === id);
+    if (producto) {
+        producto.cantidad++;
+        guardarCarrito();
+        renderCarrito();
 
+    }
+}
+function disminuirCantidad(id) {
+    const producto = carrito.find(item => item.id === id);
+    if (producto) {
+        producto.cantidad--;
+        if (producto.cantidad <= 0) {
+            carrito = carrito.filter(item => item.id !== id)
+        }
+
+        guardarCarrito();
+        renderCarrito();
+
+    }
+}
+
+function eliminarProducto(id) {
+    carrito = carrito.filter(item => item.id !==id);
+    guardarCarrito();
+    renderCarrito();
+}
+
+
+
+function mostrarTotal() {
+    const total = carrito.reduce((acumulador, item) => {
+        return acumulador + item.precio * item.cantidad;
+    }, 0);
+    totalElemento.textContent = `Total $${total}`;
+}
+
+
+
+function vaciarCarrito() {
+    carrito = [];
+    localStorage.removeItem("carrito");
+    renderCarrito();
+}
+
+
+contenedor.addEventListener("click", (e) => {
+    const id = Number(e.target.dataset.id);
+
+    if(e.target.classList.contains("sumar")) {
+        aumentarCantidad(id);
+    }
+    
+    if(e.target.classList.contains("restar")) {
+        disminuirCantidad(id);
+    }
+
+    if(e.target.classList.contains("eliminar")) {
+        eliminarProducto(id);
+    }
+
+});
 
 function renderCarrito() {
 
@@ -30,24 +92,20 @@ function renderCarrito() {
         
     });
     mostrarTotal();
+
+    carrito.forEach(item => {
+        const div = document.createElement("div");
+
+        div.innerHTML =`
+        <p>${item.nombre} - $${item.precio}</p>
+        <button class="restar" data-id="${item.id}">-</button>
+        <span>${item.cantidad}</span>
+        <button class="sumar" data-id="${item.id}">+</button>
+        <button class="eliminar" data-id="${item.id}">ELIMINAR</button>
+        `;
+        contenedor.appendChild(div)
+    })
 }
-
-function mostrarTotal() {
-    const total = carrito.reduce((acumulador, item) => {
-        return acumulador + item.precio * item.cantidad;
-    }, 0);
-    totalElemento.textContent = `Total $${total}`;
-}
-
-
-
-function vaciarCarrito() {
-    carrito = [];
-    localStorage.removeItem("carrito");
-    renderCarrito();
-}
-
-
 
 botonVaciar.addEventListener("click", vaciarCarrito);
 
